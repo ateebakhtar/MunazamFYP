a3b120ec34ab96f616daf115dec1d635dd929733
ateebakhtar <ateebakhtar1997@gmail.com>
2020-01-31 17:51:18 +0500

    created background app meeting

---
+++ app/src/main/java/com/example/munazamfyp/Connections/AddMeetingConnections.java
@@ -0,0 +1,86 @@
+package com.example.munazamfyp.Connections;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.example.munazamfyp.DataModels.MeetingModel;
+import com.example.munazamfyp.Interfaces.LoginInterface;
+import com.example.munazamfyp.Interfaces.MeetingInterface;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public class AddMeetingConnections extends AsyncTask<Void, Void, Void>
+{
+    public AddMeetingConnections(MeetingModel mm, Context context) {
+        this.mm = mm;
+        this.cx = context;
+    }
+    MeetingModel mm;
+    Context cx;
+
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onPostExecute(Void v) {
+        // execution of result of Long time consuming operation
+        progressDialog.dismiss();
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        progressDialog = ProgressDialog.show(cx, "ProgressDialog", "Wait for "+ " seconds");
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+
+        Retrofit m = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        MeetingInterface GDS = m.create(MeetingInterface.class);
+
+        //JsonReader.setLenient(true);
+        //@GET("/meeting/{course}/{name}/{id}/{venue}/{time}/{date}/{capacity}/{topic}/{description}")
+
+        Call<String> call = GDS.addmeeting(mm.getCourse(),mm.getName(),mm.getUniid(),mm.getVenue(),mm.getTime(),mm.getDate(),mm.getCapacity(),mm.getTopic(),mm.getDescription());
+        //Call<String> call = GDS.Get(id);
+        Response<String> x = null;
+        try
+        {
+            x = call.execute();
+            System.out.println(x);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+}
